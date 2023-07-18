2f25c352d4a01394bf460b9c3e37c93525686f35
Jose-Alisson <84239112+Jose-Alisson@users.noreply.github.com>
2023-07-18 12:15:24 -0300

    Granja Nascimento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Granja-Nascimento-Angular';
+
+  products: any[] = [
+    {
+      photoUrl: 'assets//images-13.jpg',
+      name: 'Bandeja de ovos de codorna',
+      price: 5.50,
+      description: 'Bandeja com 30 ovos',
+      article: ' a '
+    },
+    {
+      photoUrl: 'assets//images-14.jpg',
+      name: 'Bandejão de ovos de galinha caipira',
+      price: 14.50,
+      description: 'Bandeja com 30 ovos',
+      article: ' o '
+    }
+  ]
+
+  comprar(message: string){
+    const numeroContato = '+5581999340962'; // Insira o número do seu contato do WhatsApp aqui
+    const mensagem = encodeURIComponent(message); // Codificar a mensagem para evitar problemas de URL
+
+    const url = `https://api.whatsapp.com/send?phone=${numeroContato}&text=${mensagem}`;
+    window.open(url);
+  }
 }
